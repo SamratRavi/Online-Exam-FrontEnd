4778e79007a9cc69075d07ebf2a49182cc9f1275
samrat <samrathrdt@gmail.com>
2023-09-19 17:53:24 +0530

    Added-Login-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 
 function App() {
   return <NavBar></NavBar>;
+  return <Login></Login>;
 }
 export default App;
